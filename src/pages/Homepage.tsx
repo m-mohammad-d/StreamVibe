cb023b27cb87bcf6d0ff4a0ab33d7cee78a262ca
silvershade1 <silvershade1000@gmail.com>
2024-11-22 18:31:32 +0330

    feat : add aos animation in hero section

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import AOS from "aos";
-import "aos/dist/aos.css"; 
+import "aos/dist/aos.css";
 
 import Categories from "../components/Categories";
 import Devices from "../components/Devices";
@@ -12,8 +12,8 @@ import TrialBanner from "../components/TrialBanner";
 function Homepage() {
   useEffect(() => {
     AOS.init({
-      duration: 1000, 
-      easing: "ease-in-out", 
+      duration: 1000,
+      easing: "ease-in-out",
     });
   }, []);
 
@@ -21,7 +21,7 @@ function Homepage() {
     <div>
       <div className="relative bg-intro mt-4 w-full h-[700px] bg-cover bg-center">
         <div className="absolute inset-0 bg-black/60 z-0"></div>
-        <div className="relative z-10">
+        <div className="relative z-10" data-aos="fade-up" data-aos-delay="100">
           <HeroSection />
         </div>
       </div>

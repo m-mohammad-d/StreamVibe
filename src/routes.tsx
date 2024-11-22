cf1a4e0f30ee8dce0c8f22ec4dfc6f61632801e0
silvershade1 <silvershade1000@gmail.com>
2024-11-22 18:55:53 +0330

    feat : add movies route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import AppLayout from "./layout/AppLayout";
 import Homepage from "./pages/Homepage";
+import MoviesPage from "./pages/MoviesPage";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
-    children: [{ path: "/", element: <Homepage /> }],
+    children: [
+      { path: "/", element: <Homepage /> },
+      { path: "/movies", element: <MoviesPage /> },
+    ],
   },
 ]);
 

a868042342b6b3a26a79b3868e0ce38b8b229e0d
silvershade1 <silvershade1000@gmail.com>
2024-11-22 21:57:45 +0330

    feat : add Aos animation in movies Page

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -1,22 +1,41 @@
-
 import VideoTrailers from "../components/VideoTrailers";
 import MoviesList from "../components/MoviesList";
 import ShowsList from "../components/ShowsList";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function MoviesPage() {
   const movieGenres = ["Action", "Comedy", "Drama", "Horror"];
   const showGenres = ["Action", "Comedy", "Drama", "Horror"];
-
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+    });
+  }, []);
   return (
     <div className="max-w-screen-2xl mx-auto px-4 py-8">
-      <VideoTrailers />
-      <h1 className="text-3xl font-bold mt-8 text-white">Movies</h1>
-      {movieGenres.map((genre) => (
-        <MoviesList key={genre} genre={genre} />
+      <div data-aos="fade-up">
+        <VideoTrailers />
+      </div>
+
+      <h1 data-aos="fade-right" className="text-3xl font-bold mt-8 text-white">
+        Movies
+      </h1>
+      {movieGenres.map((genre, index) => (
+        <div data-aos="fade-up" data-aos-delay={`${index * 100}`} key={genre}>
+          <MoviesList genre={genre} />
+        </div>
       ))}
-      <h1 className="text-3xl font-bold mt-12 text-white">Shows</h1>
-      {showGenres.map((genre) => (
-        <ShowsList key={genre} genre={genre} />
+
+      <h1 data-aos="fade-left" className="text-3xl font-bold mt-12 text-white">
+        Shows
+      </h1>
+      {showGenres.map((genre, index) => (
+        <div data-aos="fade-up" data-aos-delay={`${index * 100}`} key={genre}>
+          <ShowsList genre={genre} />
+        </div>
       ))}
     </div>
   );

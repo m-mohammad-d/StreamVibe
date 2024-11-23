d3fb9a28d16445f674f393b5cdf6ce6d53a739ff
silvershade1 <silvershade1000@gmail.com>
2024-11-23 19:33:01 +0330

    feat : add VideoTrailers in movies page

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -1,9 +1,15 @@
 import GenresSlider from "../components/GenresSlider";
 import MediaSection from "../components/MediaSection";
+import VideoTrailers from "../components/VideoTrailers";
 
 const MoviesPage = () => {
   return (
     <div className="max-w-screen-2xl mx-auto px-4 py-8">
+      <VideoTrailers
+        name="dark"
+        title="A missing child causes four families to help each other for answers. What they could not imagine is that this mystery would be connected to innumerable other secrets of the small town."
+        poster="https://image.tmdb.org/t/p/original//7CFCzWIZZcnxHke3yAQiGPWXHwF.jpg&quot"
+      />
       <div className="border-2 border-dark-15 p-4 rounded-lg">
         <GenresSlider type="movie" />
 

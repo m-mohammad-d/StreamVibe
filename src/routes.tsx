edc99ce7565047c5421279a9bde58c4064a4ec24
silvershade1 <silvershade1000@gmail.com>
2024-11-26 21:10:15 +0330

    feat : add support route

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import AppLayout from "./layout/AppLayout";
 import Homepage from "./pages/Homepage";
 import MoviesPage from "./pages/MoviesPage";
 import MovieDetailPage from "./pages/MovieDetailPage";
+import SupportPage from "./pages/SupportPage";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
       { path: "/", element: <Homepage /> },
       { path: "/movies", element: <MoviesPage /> },
       { path: "/movie/:Type/:id", element: <MovieDetailPage /> },
+      { path: "/support", element: <SupportPage /> },
     ],
   },
 ]);

62fbf2b7e9c9fe06993e5578ed1155128a80caff
silvershade1 <silvershade1000@gmail.com>
2024-11-28 19:03:16 +0330

    feat: add AOS animations to MovieDetailPage for smoother transitions

--- src/pages/MovieDetailPage.tsx
+++ src/pages/MovieDetailPage.tsx
@@ -1,4 +1,8 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import {
   useGetMovieByIdQuery,
   useGetShowByIdQuery,
@@ -26,22 +30,43 @@ const MovieDetailPage = () => {
   const { data: castData, isLoading: castLoading } = castQuery(id);
   const { data: reviewsData, isLoading: reviewsLoading } = reviewsQuery(id);
 
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+    });
+  }, []);
+
   if (mediaLoading || castLoading || reviewsLoading) return <Loader />;
 
   return (
     <div className="max-w-screen-2xl mx-auto px-4">
-      <VideoTrailers
-        poster={`https://image.tmdb.org/t/p/original/${media.poster_path}`}
-        title={media.overview}
-        name={media.title || media.name || media.original_title}
-      />
-      <div className="flex flex-col lg:flex-row justify-between gap-6 mx-4 mt-4">
+      <div data-aos="zoom-in">
+        <VideoTrailers
+          poster={`https://image.tmdb.org/t/p/original/${media.poster_path}`}
+          title={media.overview}
+          name={media.title || media.name || media.original_title}
+        />
+      </div>
+
+      <div
+        className="flex flex-col lg:flex-row justify-between gap-6 mx-4 mt-4"
+        data-aos="fade-up"
+      >
         <div className="lg:w-2/3 w-full space-y-6">
-          <MovieDescription description={media.overview} />
-          <MovieCast cast={castData.cast} />
-          <MovieReviews reviews={reviewsData.results} />
+          <div data-aos="fade-right">
+            <MovieDescription description={media.overview} />
+          </div>
+          <div data-aos="fade-right" data-aos-delay="200">
+            <MovieCast cast={castData.cast} />
+          </div>
+          <div data-aos="fade-right" data-aos-delay="400">
+            <MovieReviews reviews={reviewsData.results} />
+          </div>
         </div>
-        <div className="lg:w-1/3 w-full">
+
+        <div className="lg:w-1/3 w-full" data-aos="fade-left">
           <MovieInfo
             year={media.first_air_date || media.release_date}
             languages={media.original_language}

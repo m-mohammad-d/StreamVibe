113739c720a9eb3add7c584be74ea391da8cf026
silvershade1 <silvershade1000@gmail.com>
2024-11-28 19:33:54 +0330

    Add AOS animations to SubscriptionsPage

--- src/pages/SubscriptionsPage.tsx
+++ src/pages/SubscriptionsPage.tsx
@@ -1,11 +1,26 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import PlanComparison from "../components/PlanComparison";
 import Subscription from "../components/Subscription";
 
 function SubscriptionsPage() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+    });
+  }, []);
+
   return (
     <div className="max-w-screen-2xl mx-auto px-4">
-      <Subscription />
-      <PlanComparison />
+      <div data-aos="fade-up">
+        <Subscription />
+      </div>
+      <div data-aos="fade-up" data-aos-delay="200">
+        <PlanComparison />
+      </div>
     </div>
   );
 }

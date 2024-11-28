8623db3f5f7b8b41ff9271d3c49aa2482018f564
silvershade1 <silvershade1000@gmail.com>
2024-11-28 18:56:09 +0330

    feat: add individual AOS animations for components on MoviesPage

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -1,44 +1,86 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import GenresSlider from "../components/GenresSlider";
 import MediaSection from "../components/MediaSection";
 import VideoTrailers from "../components/VideoTrailers";
 
 const MoviesPage = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+    });
+  }, []);
+
   return (
     <div className="max-w-screen-2xl mx-auto px-4 py-8">
-      <VideoTrailers
-        name="dark"
-        title="A missing child causes four families to help each other for answers. What they could not imagine is that this mystery would be connected to innumerable other secrets of the small town."
-        poster="https://image.tmdb.org/t/p/original//7CFCzWIZZcnxHke3yAQiGPWXHwF.jpg&quot"
-      />
-      <div className="border-2 border-dark-15 p-4 rounded-lg">
-        <GenresSlider type="movie" />
-
-        <MediaSection
-          title="Popular Movies"
-          type="popular"
-          mediaType="movies"
-        />
-        <MediaSection
-          title="Upcoming Movies"
-          type="upcoming"
-          mediaType="movies"
-        />
-        <MediaSection
-          title="Top Rated Movies"
-          type="top_rated"
-          mediaType="movies"
+      <div data-aos="zoom-in">
+        <VideoTrailers
+          name="dark"
+          title="A missing child causes four families to help each other for answers. What they could not imagine is that this mystery would be connected to innumerable other secrets of the small town."
+          poster="https://image.tmdb.org/t/p/original//7CFCzWIZZcnxHke3yAQiGPWXHwF.jpg&quot"
         />
       </div>
-      <div className="border-2 border-dark-15 p-4 rounded-lg">
+
+      <div
+        className="border-2 border-dark-15 p-4 rounded-lg"
+        data-aos="fade-right"
+      >
+        <GenresSlider type="movie" />
+
+        <div data-aos="fade-up">
+          <MediaSection
+            title="Popular Movies"
+            type="popular"
+            mediaType="movies"
+          />
+        </div>
+        <div data-aos="fade-up" data-aos-delay="200">
+          <MediaSection
+            title="Upcoming Movies"
+            type="upcoming"
+            mediaType="movies"
+          />
+        </div>
+        <div data-aos="fade-up" data-aos-delay="400">
+          <MediaSection
+            title="Top Rated Movies"
+            type="top_rated"
+            mediaType="movies"
+          />
+        </div>
+      </div>
+
+      <div
+        className="border-2 border-dark-15 p-4 rounded-lg"
+        data-aos="fade-left"
+      >
         <GenresSlider type="tv" />
 
-        <MediaSection title="Popular Shows" type="popular" mediaType="shows" />
-        <MediaSection title="On The Air" type="on_the_air" mediaType="shows" />
-        <MediaSection
-          title="Top Rated Shows"
-          type="top_rated"
-          mediaType="shows"
-        />
+        <div data-aos="fade-up">
+          <MediaSection
+            title="Popular Shows"
+            type="popular"
+            mediaType="shows"
+          />
+        </div>
+        <div data-aos="fade-up" data-aos-delay="200">
+          <MediaSection
+            title="On The Air"
+            type="on_the_air"
+            mediaType="shows"
+          />
+        </div>
+        <div data-aos="fade-up" data-aos-delay="400">
+          <MediaSection
+            title="Top Rated Shows"
+            type="top_rated"
+            mediaType="shows"
+          />
+        </div>
       </div>
     </div>
   );
